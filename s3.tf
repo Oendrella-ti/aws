9476a57288989e81e4e57056cdc4ba9cc357e70c
Oendrella <anacatlove97@gmail.com>
2026-03-08 13:56:04 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  backend "s3" {
+    bucket         = "tapari09081998"  # S3 bucket name
+    key            = "terraform.tfstate"  # path inside bucket
+    region         = "us-east-1"
+    encrypt        = true  # encrypt state at rest
+    dynamodb_table = "terraform-locks"  # optional table for state locking
+  }
+}
