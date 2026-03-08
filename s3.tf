566017d2591ada78d19c129df074f907bf9df025
Oendrella <anacatlove97@gmail.com>
2026-03-08 16:36:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.4.0"
-
   backend "s3" {
     bucket         = "tapari09081998"  # S3 bucket name
     key            = "terraform.tfstate"  # path inside bucket
